bbc41ab7c3d446baf3ec68dc8fcabbb730e6e6d1
Anatolij Goga <anzefal@gmail.com>
2022-12-07 19:42:32 +0200

    Push

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,10 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsItemEl = document.createElement('li');
+ingredientsItemEl.classList.add('item');
+ingredientsItemEl.textContent = `lol`;
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+ingredientsEl.appendChild(ingredientsItemEl);
+console.log(ingredientsItemEl);
