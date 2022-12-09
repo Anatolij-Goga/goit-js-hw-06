857752ebdca12cf5b26edb71334022ddf4fe6719
Anatolij Goga <anzefal@gmail.com>
2022-12-09 22:44:19 +0200

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,8 @@
+const counterValue = document.querySelector('#value');
+
+console.log(counterValue);
+
+const actions = document.querySelectorAll('#counter button');
+
+console.log(actions[0].dataset.action);
+console.log(actions[1].dataset.action);
